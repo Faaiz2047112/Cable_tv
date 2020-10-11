6d1f5744e2914a553c0ac7de7aa2c6168200910f
Faaiz2047112 <69455816+Faaiz2047112@users.noreply.github.com>
2020-10-11 17:53:24 +0530

    Update customer.java.java

--- customer.java.java
+++ customer.java.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
-class customer
+abstract class customer
 {
 	int Sr_no;
 	int id;
@@ -13,7 +13,7 @@ class customer
 	{
 		country = "INDIA";
 	}
-	public void read()
+	public  void read()
 	{
 			Scanner scn = new Scanner(System.in);
 			Random rand = new Random();
@@ -37,6 +37,7 @@ class customer
 		System.out.println("mobile number"+this.mobile_number);
 		System.out.println("address - "+this.address);
 		System.out.println("email - "+this.email);
+		System.out.println("country - "+this.country);
 	}
 	
 }
@@ -44,9 +45,10 @@ class operator extends customer
 {
 		int Ope_id;
 		String Ope_name;
-		int Ope_Mobilenumber;
+		final int Ope_Mobilenumber=9632587;
 		String Ope_address;
 		String Ope_email;
+	
 	
 	public void read()
 	{
@@ -56,8 +58,6 @@ class operator extends customer
 		Ope_id=Integer.parseInt(scn.nextLine());
 		System.out.println("enter the Operator name ");
 		Ope_name=scn.nextLine();
-		System.out.println("enter the Operator mobile mumber");
-		Ope_Mobilenumber=Integer.parseInt(scn.nextLine());
 		System.out.println("enter the operator address");
 		Ope_address=scn.nextLine();
 		System.out.println("enter the operator email");
@@ -68,11 +68,11 @@ class operator extends customer
 			super.display();
 			System.out.println("operator id - "+this.Ope_id);
 			System.out.println("operator name - "+this.Ope_name);
-			System.out.println("operator mobile number -"+this.Ope_Mobilenumber);
+			System.out.println("operator mobile_number - "+this.Ope_Mobilenumber);
 			System.out.println("operator address - "+this.Ope_address);
 			System.out.println("operator email - "+this.Ope_email);
 	}
-	public void display(int ope_id)
+	public void display(int ope_id) //funtion overloading with one argument//
 	{
 			super.display();
 			System.out.println("operator id - "+this.Ope_id);
@@ -81,48 +81,35 @@ class operator extends customer
 			System.out.println("operator address - "+this.Ope_address);
 			System.out.println("operator email - "+this.Ope_email);
 	}
-}
-class channels extends operator
-{
-	String Chnl_name;
-	int Chnl_number;
-	String Chnl_type;
-	public void read()
+	operator()
 	{
-		super.read();
-		Scanner scn = new Scanner(System.in);
-		System.out.println("enter the channel name : ");
-		Chnl_name=scn.nextLine();
-		System.out.println("enter the channel number :");
-		Chnl_number=Integer.parseInt(scn.nextLine());
-		System.out.println("enter the Channel type : ");
-		Chnl_type=scn.nextLine();
+		
 	}
-	public void display()
+	operator(String j) // Constructor overloading
 	{
-			super.display();
-			System.out.println("Chanal ame -"+this.Chnl_name);
-			System.out.println("Chanal number -"+this.Chnl_number);
-			System.out.println("Channel type -"+this.Chnl_type);
+			String i = "ope_gen";
+			Scanner in= new Scanner(System.in);
+			System.out.println("enter the customer gender");
+			System.out.println(i);
 	}
 }
+
 public class Customer1
 {
 		
 	public static void main(String args[])
 	{
 		int n,i;
-		channels obj=new channels();
-		System.out.print("Enter the number of customer:");
-		Scanner input = new Scanner(System.in);
-		n=input.nextInt();
-		channels[] gs=new channels[n];
+		operator obj=new operator();
+		n=Integer.parseInt(args[0]);
+		operator[] gs=new operator[n];
 
 		for(i=0;i<n;i++)
 		{
-			gs[i]= new channels();
+			gs[i]= new operator();
 			System.out.println("customer Details:"+(i+1));
 			gs[i].read();
+			gs[i].display();
 		}
 	}
 }
